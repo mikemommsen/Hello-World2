91c4af5b8d32add7773d9cd4a1f51fa5b86a0317
mikemommsen <mikemommsen@gmail.com>
2013-03-12 16:13:24 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
+import sys, os, shutil
 
 def this(x):
   return x
+  
+def rundir(indir):
+  os.chdir(indir)
+  files = os.listdir('.')
+  for x in files:
+    processfile(x)
+  
+def main():
+  indir = sys.argv[1]
+  rundir(indir)
+
+if __name__ == '__main__':
+  main()
