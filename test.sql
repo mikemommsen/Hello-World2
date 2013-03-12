015e598b1c6efd1717035db4faa34d75e775b73c
mikemommsen <mikemommsen@gmail.com>
2013-03-12 16:57:07 -0500

    Update test.sql

--- test.sql
+++ test.sql
@@ -6,4 +6,6 @@ create table tester (
 ;
 insert into tester values('a','b',3);
 insert into tester values('b','a',4);
+insert into tester values('b','a',5);
+insert into tester values('b','a',7);
 commit;
